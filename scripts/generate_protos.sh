b98b32ef13eabe24205537b873d2c4561d87775f
Mooseburger1 <mooseburger1@msn.com>
2023-05-29 14:33:01 -0400

    updated shell script to be directory agnostic

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -1,1 +1,3 @@
-protoc -I ../backend/grpc/proto ../backend/grpc/proto/*.proto --go_out=plugins=grpc:../
+WORKING_DIR="/home/scott/repos/sara_updated"
+
+protoc -I ${WORKING_DIR}/backend/grpc/proto ${WORKING_DIR}/backend/grpc/proto/*.proto --go_out=plugins=grpc:../
