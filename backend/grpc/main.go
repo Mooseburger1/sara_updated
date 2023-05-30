9dff9d4c7f604267b754f4ca5f06cbac1f14a790
Mooseburger1 <mooseburger1@msn.com>
2023-05-29 21:44:23 -0400

    updated main.go to shutdown servers

--- backend/grpc/main.go
+++ backend/grpc/main.go
@@ -3,14 +3,21 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
 
 	"sara_updated/backend/common"
 	photo_server "sara_updated/backend/grpc/photos/google"
 )
 
-func startServers(servers ...common.SaraServer) {
+func startServers(servers []common.SaraServer) {
 	for _, server := range servers {
 		go server.StartServer()
+	}
+}
+
+func shutdownServers(servers []common.SaraServer) {
+	for _, server := range servers {
+		server.ShutdownServer()
 	}
 }
 
@@ -18,11 +25,21 @@ func main() {
 	logger := log.New(os.Stdout, "grpc-server-manager", log.LstdFlags)
 
 	// Initialize gRPC servers
-	photoServer := photo_server.NewPhotosServer()
+	servers := []common.SaraServer{
+		photo_server.NewPhotosServer(),
+	}
 
 	// Start Servers
 	logger.Print("Starting RPC servers")
-	startServers(photoServer)
+	startServers(servers)
 
-	select {}
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	logger.Printf("Received terminate signal of %s. Performing graceful shutdown", sig)
+	shutdownServers(servers)
+
 }
