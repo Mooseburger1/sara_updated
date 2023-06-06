dbe33863dba88f2b2875d7d8e7c9d2a29a1733af
Mooseburger1 <mooseburger1@msn.com>
2023-06-05 23:06:15 -0400

    implemented the ListAlbums method of the grpc server

--- backend/grpc/photos/google/server_api.go
+++ backend/grpc/photos/google/server_api.go
@@ -2,9 +2,20 @@ package photo_server
 
 import (
 	"context"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 
 	"sara_updated/backend/common"
 	"sara_updated/backend/grpc/proto/photos"
+
+	"github.com/golang/protobuf/jsonpb"
+)
+
+const (
+	ALBUMS_ENDPOINT = "https://photoslibrary.googleapis.com/v1/albums"
 )
 
 // GPhotosAPI is the implementation of the
@@ -13,12 +24,14 @@ import (
 // * GetAlbumMedia service
 type GPhotosAPI struct {
 	clientCreator common.ClientFunc
+	logger        *log.Logger
 }
 
 // Builder for instantiating a GPhotosAPI
 func NewGPhotosApiStub(cf common.ClientFunc) *GPhotosAPI {
 	return &GPhotosAPI{
 		clientCreator: cf,
+		logger:        log.New(os.Stdout, "google-photos-grpc-server", log.LstdFlags),
 	}
 }
 
@@ -28,9 +41,40 @@ func NewGPhotosApiStub(cf common.ClientFunc) *GPhotosAPI {
 func (g *GPhotosAPI) ListAlbums(ctx context.Context,
 	rpc *photos.AlbumListRequest) (*photos.AlbumsInfo, error) {
 
-	//TODO
-	return nil, nil
+	client, err := g.clientCreator(rpc.GetOauthInfo())
+	if err != nil {
+		g.logger.Printf("Error creating oauth http client for grpc photos request: %v", err)
+		return nil, common.CreateClientCreationError(err).Err()
+	}
 
+	req, err := http.NewRequest("GET", ALBUMS_ENDPOINT, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	addQueryParams(req, rpc)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		g.logger.Printf("Call to List Albums returned status code %v, not %v", resp.StatusCode, http.StatusOK)
+		bodyBytes, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			panic(err)
+		}
+		return nil, common.CreateErrorResponseError(resp.StatusCode, bodyBytes).Err()
+	}
+
+	respRpc := &photos.AlbumsInfo{}
+	err = jsonpb.Unmarshal(resp.Body, respRpc)
+
+	return respRpc, nil
 }
 
 // GetAlbumMedia is a RPC service endpoint. It receives a
@@ -42,4 +86,11 @@ func (g *GPhotosAPI) GetAlbumMedia(ctx context.Context,
 
 	//TODO
 	return nil, nil
+}
+
+func addQueryParams(req *http.Request, rpc *photos.AlbumListRequest) {
+	query := req.URL.Query()
+	query.Add("pageToken", rpc.GetPageToken())
+	query.Add("pageSize", strconv.Itoa(int(rpc.GetPageSize())))
+	req.URL.RawQuery = query.Encode()
 }
