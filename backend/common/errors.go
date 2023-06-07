bd1e8116da380e0b939a028f9b9ef766bd1a1009
Mooseburger1 <mooseburger1@msn.com>
2023-06-06 23:10:34 -0400

    update common errors

--- backend/common/errors.go
+++ backend/common/errors.go
@@ -1,46 +1,23 @@
 package common
 
 import (
-	"encoding/json"
-	"fmt"
+	"errors"
 
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func CreateClientCreationError(err error) *status.Status {
-	st := status.New(codes.InvalidArgument, "Client creation error")
-	desc := fmt.Sprintf("Error creating client for making REST calls to Google photos API: %s", err)
-	v := &errdetails.ErrorInfo{Reason: desc}
-	st, err = st.WithDetails(v)
-	if err != nil {
-		panic(fmt.Sprintf("Unexpected error attaching metadata: %v", err))
-	}
-	return st
+var httpClientCreationError = errors.New("Http client creation error")
+
+func ClientCreationError() error {
+	return httpClientCreationError
 }
 
-func CreateErrorResponseError(statusCode int, response []byte) *status.Status {
-	var rpcErrCode codes.Code
-	var desc errResponse
-
-	json.Unmarshal(response, &desc)
-
+func RpcErrorResponse(statusCode int, desc string) *status.Status {
 	switch statusCode {
 	case 400:
-		rpcErrCode = codes.InvalidArgument
+		return status.New(codes.InvalidArgument, desc)
 	default:
-		rpcErrCode = codes.InvalidArgument
+		return status.New(codes.InvalidArgument, desc)
 	}
-
-	return status.New(rpcErrCode, desc.Error.Message)
-}
-
-type errResponse struct {
-	Error errDetails `json:"error"`
-}
-
-type errDetails struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
 }
